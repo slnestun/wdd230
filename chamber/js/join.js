f22aa17465e5f3c63e04c62813ca244fc163c3e1
Nestor <nestor.rivera@davinci.edu.ar>
2022-03-10 13:55:07 -0300

    finis join page

---
+++ chamber/js/join.js
@@ -0,0 +1,12 @@
+let cuerrentDate = new Date();
+
+// Set the value of the "date" field
+document.getElementById("date").value = cuerrentDate.toDateString();
+
+// Set the value of the "time" field
+let hours = cuerrentDate.getHours();
+let mins = cuerrentDate.getMinutes();
+let seconds = cuerrentDate.getSeconds();
+document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
+
+console.log(cuerrentDate);
