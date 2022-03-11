def301d3e7e67f7f6c33966e0a26a680013dcc23
Nestor <nestor.rivera@davinci.edu.ar>
2022-03-11 02:27:06 -0300

    show form data

--- chamber/js/thankyou.js
+++ chamber/js/thankyou.js
@@ -1,13 +1,24 @@
 window.addEventListener("load", () => {
-  // Via Query parameters - GET
   const params = new URL(document.location).searchParams;
   const name = params.get("fname");
-  const surname = params.get("lsurname");
-
-  // Via local Storage
-  /* const name = localStorage.getItem('NAME');
-    const surname = localStorage.getItem('SURNAME'); */
+  const lname = params.get("lname");
+  const phone = params.get("phone");
+  const email = params.get("email");
+  const business = params.get("business");
+  const position = params.get("position");
+  const description = params.get("description");
+  const currentDate = params.get("currentDate");
+  const currentTime = params.get("currentTime");
+  const audience = params.get("audience");
 
   document.getElementById("name-form").innerHTML = name;
-  document.getElementById("lastName-form").innerHTML = surname;
+  document.getElementById("lastName-form").innerHTML = lname;
+  document.getElementById("cellphone-form").innerHTML = phone;
+  document.getElementById("email-form").innerHTML = email;
+  document.getElementById("business-form").innerHTML = business;
+  document.getElementById("business-position").innerHTML = position;
+  document.getElementById("description-form").innerHTML = description;
+  document.getElementById("membership-form").innerHTML = audience;
+  document.getElementById("date-form").innerHTML = currentDate;
+  document.getElementById("time-form").innerHTML = currentTime;
 });
