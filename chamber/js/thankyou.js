5f547f73601beb7597451c798371599bc6c9f463
Nestor <nestor.rivera@davinci.edu.ar>
2022-03-11 01:50:14 -0300

    more changes

--- chamber/js/thankyou.js
+++ chamber/js/thankyou.js
@@ -1,8 +1,8 @@
 window.addEventListener("load", () => {
   // Via Query parameters - GET
   const params = new URL(document.location).searchParams;
-  const name = params.get("name");
-  const surname = params.get("surname");
+  const name = params.get("fname");
+  const surname = params.get("lsurname");
 
   // Via local Storage
   /* const name = localStorage.getItem('NAME');
