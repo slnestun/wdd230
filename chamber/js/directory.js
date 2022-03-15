ab6c1cd47fa3f7ed1064c70f090a6c27931df81d
slnestun <riv21007@byui.edu>
2022-03-15 17:37:01 -0300

    final versiòn of chamber

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,27 +1,50 @@
+const div = document.querySelector('.directory-buttons');
+const cardsButton = document.getElementById('card-button');
+const listButton = document.getElementById('list-button');
+const cards = document.getElementById('cards');
+const list = document.getElementById('list');
+
+cardsButton.addEventListener('click', () => {
+  list.classList.add('hidden');
+  cards.classList.remove('hidden');
+}, false);
+
+listButton.addEventListener('click', () => {
+  cards.classList.add('hidden');
+  list.classList.remove('hidden');
+}, false);
+
+if (window.innerWidth >= 520 && window.innerWidth <= 1120) {
+  cards.classList.add('hidden');
+  list.classList.remove('hidden');
+}else{
+  cards.classList.remove('hidden');
+  list.classList.add('hidden');
+};
+
+
 const requestURL = "https://slnestun.github.io/wdd230/chamber/data/data.json";
-const cards = document.querySelector(".cards");
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    //loop integraded in the promise
-    console.table(jsonObject); // temporary checking for valid response and data parsing
-    const members = jsonObject["members"];
-    members.forEach(displayBusiness);
+    const member = jsonObject["members"];
+    member.forEach(displayCards);
+    member.forEach(displayList);
   });
 
-function displayBusiness(member) {
+function displayCards(member) {
   //Create elements to add info from json
-  let card = document.createElement("section");
+  let cardView = document.createElement("section");
   let h2 = document.createElement("h2");
   let image = document.createElement("img");
   let address = document.createElement("p");
   let phone = document.createElement("p");
   let site = document.createElement("a");
 
-  card.setAttribute("class", "images");
+  cardView.setAttribute("class", "images");
   h2.textContent = `${member.name}`;
   image.setAttribute("src", member.logo);
   image.setAttribute("alt", `${member.name} Logo`);
@@ -32,9 +55,35 @@ function displayBusiness(member) {
   site.target = "_blank";
   site.textContent = `${member.website}`;
 
-  card.appendChild(image);
-  card.appendChild(address);
-  card.appendChild(phone);
-  card.appendChild(site);
-  cards.appendChild(card);
+  cardView.appendChild(image);
+  cardView.appendChild(address);
+  cardView.appendChild(phone);
+  cardView.appendChild(site);
+  document.querySelector("#cards").appendChild(cardView);
 }
+
+
+function displayList(member) {
+  //Create elements to add to the document
+  let listview = document.createElement('section');
+  let h4 = document.createElement('h3');
+  let p1 = document.createElement('p');
+  let p2 = document.createElement('p');
+  let a = document.createElement('a');
+
+  //Add text content to the directory cards
+  h4.textContent = `${member.name}`;
+  p1.textContent = `${member.address}`;
+  p2.textContent = `${member.phone}`;
+  a.textContent = `${member.website}`;
+  a.setAttribute('href', member.website);
+
+  //Add/append the h2 and image to the section(card)
+  listview.appendChild(h4);
+  listview.appendChild(p1);
+  listview.appendChild(p2);
+  listview.appendChild(a);
+  document.querySelector('#list').appendChild(listview);
+
+}
+
