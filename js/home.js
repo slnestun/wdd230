bbac1a0a8e63805ff4f12a14b7b1dbb166415bb0
Nestor <nestor.rivera@davinci.edu.ar>
2022-03-17 01:34:49 -0300

    fix home

--- js/home.js
+++ js/home.js
@@ -1,81 +1,82 @@
 //create a veriable to store lessons names and links
-let myLessons = {}
+let myLessons = {};
 
 myLessons = {
-    weekLessons: [{
-            lessons: 'Lesson 02: ',
-            linkName: 'Design Principles Document',
-            link: './lesson2/index.html'
-        },
-        {
-            lessons: 'Lesson 03: ',
-            linkName: 'Website Planning Document',
-            link: './lesson3/index.html'
-        },
-        {
-            lessons: 'Lesson 04: ',
-            linkName: 'Chamber Of Comerce',
-            link: './chamber/index.html'
-        },
-        {
-            lessons: 'Lesson 05: ',
-            linkName: 'BOM',
-            link: './bom/index.html'
-        },
-        {
-            lessons: 'Lesson 06: ',
-            linkName: 'Title',
-            link: '#'
-        },
-        {
-            lessons: 'Lesson 07: ',
-            linkName: 'Progressive Loading of Images',
-            link: './lesson7/lazyload.html'
-        },
-        {
-            lessons: 'Lesson 08: ',
-            linkName: 'Table Build',
-            link: './lesson8/tablebuild.html'
-        },
-        {
-            lessons: 'Lesson 09: ',
-            linkName: 'Working with JSON data and the Fetch API',
-            link: './lesson9/prophets.html'
-        },
-        {
-            lessons: 'Lesson 10: ',
-            linkName: 'Title',
-            link: '#'
-        },
-        {
-            lessons: 'Lesson 11: ',
-            linkName: 'Title',
-            link: '#'
-        }
-    ]
+  weekLessons: [
+    {
+      lessons: "Lesson 02: ",
+      linkName: "Design Principles Document",
+      link: "./lesson2/index.html",
+    },
+    {
+      lessons: "Lesson 03: ",
+      linkName: "Website Planning Document",
+      link: "./lesson3/index.html",
+    },
+    {
+      lessons: "Lesson 04: ",
+      linkName: "Chamber Of Comerce",
+      link: "./chamber/index.html",
+    },
+    {
+      lessons: "Lesson 05: ",
+      linkName: "BOM",
+      link: "./bom/index.html",
+    },
+    {
+      lessons: "Lesson 06: ",
+      linkName: "Responsive Image Activity",
+      link: "./chamber/index.html",
+    },
+    {
+      lessons: "Lesson 07: ",
+      linkName: "Progressive Loading of Images",
+      link: "./lesson7/lazyload.html",
+    },
+    {
+      lessons: "Lesson 08: ",
+      linkName: "Table Build",
+      link: "./lesson8/tablebuild.html",
+    },
+    {
+      lessons: "Lesson 09: ",
+      linkName: "Working with JSON data and the Fetch API",
+      link: "./lesson9/prophets.html",
+    },
+    {
+      lessons: "Lesson 10: ",
+      linkName: "Weather API updateitle",
+      link: "./lesson10/weather-api.html",
+    },
+    {
+      lessons: "Lesson 11: ",
+      linkName: "Chamber Contact Page",
+      link: "./chamber/contact.html",
+    },
+  ],
 };
 
 //foreach to create li and a ahref elements
-let weeklyLessons = document.getElementById('lessons');
+let weeklyLessons = document.getElementById("lessons");
 myLessons.weekLessons.forEach((weekLessons) => {
-    let listLesson = document.createElement('li');
-    listLesson.textContent = weekLessons.lessons;
-    weeklyLessons.appendChild(listLesson);
+  let listLesson = document.createElement("li");
+  listLesson.textContent = weekLessons.lessons;
+  weeklyLessons.appendChild(listLesson);
 
-    let listLinks = document.createElement('a');
-    listLinks.innerText = weekLessons.linkName;
-    listLinks.href = weekLessons.link;
-    listLinks.target = '_blank';
-    listLesson.appendChild(listLinks);
-})
+  let listLinks = document.createElement("a");
+  listLinks.innerText = weekLessons.linkName;
+  listLinks.href = weekLessons.link;
+  listLinks.target = "_blank";
+  listLesson.appendChild(listLinks);
+});
 //console.log(weeklyLessons)
 
 //create an object to store last date data
 let oLastModif = new Date(document.lastModified);
 let currentYear = oLastModif.getFullYear();
-document.querySelector('#year').innerText = currentYear;
+document.querySelector("#year").innerText = currentYear;
 
 let dateFormated = oLastModif.toLocaleString();
 //console.log(dateFormated)
 
-document.getElementById('currentDate').innerText = dateFormated;
+document.getElementById("currentDate").innerText = dateFormated;
