6640fd185de121b64f16fcba9af73eb1ca137363
Nestor <nestor.rivera@davinci.edu.ar>
2022-05-15 00:22:56 -0300

    update home chamber

--- lesson8/tablebuild.js
+++
@@ -1,9 +0,0 @@
-//create an object to store last date data
-let oLastModif = new Date(document.lastModified);
-let currentYear = oLastModif.getFullYear();
-document.querySelector('#year').innerText = currentYear;
-
-let dateFormated = oLastModif.toLocaleString();
-//console.log(dateFormated)
-
-document.getElementById('currentDate').innerText = dateFormated;
