8cb8ab11f83f15dd19d257f46dad79f9b605dad9
Nestor <nestor.rivera@davinci.edu.ar>
2022-05-18 22:33:37 -0300

    bom activity

--- bom/js/home.js
+++ bom/js/home.js
@@ -1,23 +1,23 @@
-const list = document.querySelector('ul');
-      const input = document.querySelector('input');
-      const button = document.querySelector('button');
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
 
-      button.onclick = function() {
-        let myItem = input.value;
-        input.value = '';
+button.onclick = function () {
+  let myItem = input.value;
+  input.value = "";
 
-        const listItem = document.createElement('li');
-        const listText = document.createElement('span');
-        const listBtn = document.createElement('button');
+  const listItem = document.createElement("li");
+  const listText = document.createElement("span");
+  const listBtn = document.createElement("button");
 
-        listItem.appendChild(listText);
-        listText.textContent = myItem;
-        listItem.appendChild(listBtn);
-        listBtn.textContent = '❌';
-        list.appendChild(listItem);
+  listItem.appendChild(listText);
+  listText.textContent = myItem;
+  listItem.appendChild(listBtn);
+  listBtn.textContent = "❌";
+  list.appendChild(listItem);
 
-        listBtn.onclick = function(e) {
-          list.removeChild(listItem);
-        }
-        input.focus();
-      }
+  listBtn.onclick = function (e) {
+    list.removeChild(listItem);
+  };
+  input.focus();
+};
