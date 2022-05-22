9eecf394e8597067fe6622bedfe76f48dde0d828
Nestor <nestor.rivera@davinci.edu.ar>
2022-05-21 23:51:34 -0300

    complete home chamber and add json for spotlight

---
+++ chamber/js/windchill.js
@@ -0,0 +1,35 @@
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?id=3433955&units=Metric&appid=62b017d0bc368f491774a1b152049f7f";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    //console.log(jsObject);
+    document.querySelector(".temperature").textContent = Math.round(
+      jsObject.main.temp
+    );
+    const iconsrc = `https://openweathermap.org/img/wn/${jsObject.weather[0].icon}@2x.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector("#weathericon").setAttribute("src", iconsrc);
+    document.querySelector("#weathericon").setAttribute("alt", desc);
+    document.querySelector("#weathericon").setAttribute("height", 100);
+    document.querySelector("#weathericon").setAttribute("width", 100);
+    document.querySelector("figcaption").textContent = desc;
+    document.querySelector(".windspeed").textContent =
+      jsObject.wind.speed.toFixed(1);
+
+    let t = jsObject.main.temp;
+    let w = jsObject.wind.speed;
+    if (t <= 10 && w > 4.83) {
+      const windchill =
+        13.12 +
+        0.6215 * t -
+        11.37 * Math.pow(w, 0.16) +
+        0.3965 * t * Math.pow(w, 0.16);
+      document.querySelector(".windchill").innerHTML = `${Math.round(
+        windchill
+      )}&#176;C`;
+    } else {
+      document.querySelector(".windchill").innerHTML = "N/A";
+    }
+  });
