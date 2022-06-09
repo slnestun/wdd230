d38ba711df01fd25c0f5b7019fb9b461726e7ecf
slnestun <riv21007@byui.edu>
2022-06-08 23:11:46 -0300

    add hidden to form

---
+++ chamber/js/join.js
@@ -0,0 +1,6 @@
+let currentDate = new Date().toString();
+currentDate = currentDate.substring(0,24)
+//console.log(currentDate)
+
+document.getElementById('current-date').value = currentDate;
+
---
+++ chamber/js/thankyou.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", () => {
+  const params = new URL(document.location).searchParams;
+  const name = params.get("fname");
+  const lname = params.get("lname");
+  const phone = params.get("phone");
+  const email = params.get("email");
+  const business = params.get("business");
+  const position = params.get("position");
+  const description = params.get("description");
+  const audience = params.get("audience");
+  const dateform = params.get("current-date");
+
+  document.getElementById("name-form").innerHTML = name;
+  document.getElementById("lastName-form").innerHTML = lname;
+  document.getElementById("cellphone-form").innerHTML = phone;
+  document.getElementById("email-form").innerHTML = email;
+  document.getElementById("business-form").innerHTML = business;
+  document.getElementById("business-position").innerHTML = position;
+  document.getElementById("description-form").innerHTML = description;
+  document.getElementById("membership-form").innerHTML = audience;
+  document.getElementById("date-form").innerHTML = dateform;
+});
