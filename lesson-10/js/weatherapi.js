c4fe12ead37dce509da1458247d097c46be6ba11
slnestun <riv21007@byui.edu>
2022-06-17 15:23:52 -0300

    finish api

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -1,3 +1,7 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
 const apiURL =
   "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=Imperial&appid=62b017d0bc368f491774a1b152049f7f";
 
@@ -5,12 +9,11 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.querySelector("#current-temp").textContent =
-      jsObject.main.temp.toFixed(1);
+    currentTemp.innerHTML =`<strong>${jsObject.main.temp.toFixed(0)}</strong>`;
     const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     const desc = jsObject.weather[0].description;
-    document.querySelector("#icon-src").textContent = iconsrc;
-    document.querySelector("#weathericon").setAttribute("src", iconsrc);
-    document.querySelector("#weathericon").setAttribute("alt", desc);
-    document.querySelector("figcaption").textContent = desc;
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", desc);
+    captionDesc.textContent = desc;
+    document.querySelector('#icon-src').textContent = iconsrc;
   });
